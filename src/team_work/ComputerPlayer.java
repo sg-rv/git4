b07a2f08a9c526a16783b04cf6d45042bd525da8
Aleksandr <al.tukanov@gmail.com>
2023-06-14 23:06:24 +0400

    Update ComputerPlayer.java

--- src/team_work/ComputerPlayer.java
+++ src/team_work/ComputerPlayer.java
@@ -9,5 +9,11 @@ public class ComputerPlayer extends Player {
 
     public void makeStep(String text){
         //TODO
+         Random rnd = new Random();
+
+       
+        int row = rnd.nextInt(0,arr.length);
+        int col = rnd.nextInt(0,arr.length);
+        arr[row][col] = symbol;
     }
 }
