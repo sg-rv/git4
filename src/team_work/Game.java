452d6f8662dc7f4727d4ce68ddee37e7a4c8c997
Maxim Nguyen <mxng_o@yahoo.com>
2023-06-14 22:10:22 +0300

    сделал новое меню

--- src/team_work/Game.java
+++ src/team_work/Game.java
@@ -11,23 +11,40 @@ public class Game {
             }
         }
     }
-    //Good method:
     public void startGame() {
         String input = JOptionPane.showInputDialog(null,
-                "1. Человек vs Человек\n");
+                "1. Человек vs Человек\n" +
+                        "2. Человек vs Компьютер\n" +
+                        "3. Компьютер vs Компьютер");
         if (input == null) return;
         Player player1=null;
         Player player2=null;
-        if ("1".equals(input)) {
-            player1 = new HumanPlayer(arr, Symbol.X);
-            player2 = new HumanPlayer(arr, Symbol.O);
+        switch (input) {
+            case "1" -> {
+                player1 = new HumanPlayer(arr, Symbol.X);
+                player2 = new HumanPlayer(arr, Symbol.O);
+            }
+            case "2" -> {
+                player1 = new HumanPlayer(arr, Symbol.X);
+                player2 = new ComputerPlayer(arr, Symbol.O);
+            }
+            case "3" -> {
+                player1 = new ComputerPlayer(arr, Symbol.X);
+                player2 = new ComputerPlayer(arr, Symbol.O);
+            }
         }
         int step = 0;
         while (true) {
             if (step % 2 == 0) {
                 player1.makeStep(toString());
+                if(player1.getClass() == ComputerPlayer.class){
+                    JOptionPane.showMessageDialog(null, toString());
+                }
             } else {
                 player2.makeStep(toString());
+                if(player2.getClass() == ComputerPlayer.class){
+                    JOptionPane.showMessageDialog(null, toString());
+                }
             }
             step++;
         }
